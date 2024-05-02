753ec70d5f2282db90c40f66d275cde068b601d0
Ярослав <ya.staravoitav@gmail.com>
2024-05-02 21:23:51 +0300

    Added Aspect(AOP), Slf4j  and Logback

--- core/src/main/java/by/teachmeskills/service/Service_EntityService.java
+++ core/src/main/java/by/teachmeskills/service/Service_EntityService.java
@@ -3,6 +3,7 @@ package by.teachmeskills.service;
 import by.teachmeskills.api.service.ServiceRequest;
 import by.teachmeskills.api.service.ServiceResponse;
 import by.teachmeskills.entity.Service;
+import by.teachmeskills.loggerAOP.LoggingAnnotation;
 import by.teachmeskills.mapper.ServiceMapper;
 import by.teachmeskills.repository.ServiceRepository;
 import by.teachmeskills.repository.hibbernate.ServiceRepositoryInterface;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class Service_EntityService {
     private final ServiceRepository  serviceRepository;
     private final ServiceMapper mapper;
+    @LoggingAnnotation
     public ServiceResponse save(ServiceRequest serviceRequest){
         Service entity = mapper.toEntity(serviceRequest);
         serviceRepository.save(entity);
